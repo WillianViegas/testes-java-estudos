b0ea08c772a525c9d27b5ef4245835e56b81da5f
Will <willianwed98@gmail.com>
2020-07-15 13:20:10 -0300

    Utilizando @Rule para verificar diversos asserts

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -2,6 +2,7 @@ package br.ce.wcaquino.servicos;
 
 
 import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
@@ -10,7 +11,9 @@ import java.util.Date;
 
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -19,6 +22,9 @@ import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
 
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+	
 	@Test
 	public void teste() {
 	
@@ -31,10 +37,9 @@ public class LocacaoServiceTest {
 		Locacao obj = ls.alugarFilme(usuario, filme);
 		
 		//Verificacao
-		assertThat(obj.getValor(), CoreMatchers.is(CoreMatchers.equalTo(8.50)));
-		assertThat(obj.getValor(), is(not(8.10)));
+		error.checkThat(obj.getValor(), is(equalTo(8.50)));
 		
-		Assert.assertThat(DataUtils.isMesmaData(obj.getDataLocacao(), new Date()), is(true));
-		assertThat(isMesmaData(obj.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+		error.checkThat(isMesmaData(obj.getDataLocacao(), new Date()), is(true));
+		error.checkThat(isMesmaData(obj.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
 	}
 }
